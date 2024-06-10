a132700afb1459fc3653684ff79c2019695c12a3
Adissa0 <bouraimaadissa2@gmail.com>
2024-06-10 02:40:23 +0100

    add of venv .env

--- hello.py
+++ hello.py
@@ -1,15 +1,3 @@
 
-
-
-# cinetic energy
-# E = m*c**2
-def main():
-      m = int(input("m: "))
-      print("E: ", energy(m))
-      
-
-def energy(mass, c=300000000):
-      return mass*c**2
-
-main()
-
+x = input("What's your name? ")
+print("Hello, Adissa")
